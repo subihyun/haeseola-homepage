08351519487fa0c16045e683d735c7da985ce7a0
subihyun <134204008+subihyun@users.noreply.github.com>
2026-04-20 23:18:03 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/vite.svg',
+            src: 'vite.svg',
             sizes: '192x192',
             type: 'image/svg+xml'
           },
           {
-            src: '/vite.svg',
+            src: 'vite.svg',
             sizes: '512x512',
             type: 'image/svg+xml'
           }
