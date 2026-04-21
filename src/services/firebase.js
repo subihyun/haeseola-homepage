8c3b5f4fcd811ae9899834c69a001ee2ca0d29b2
haeseola <134204008+haeseola@users.noreply.github.com>
2026-04-21 12:30:52 +0900

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL, // 👈 이 이름이 deploy.yml과 일치해야 합니다!
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
+  apiKey: "AIzaSyDQuBiOZmdX1JuSzWmHEDFHdBTTTdBU934",
+  authDomain: "haeseola-a83de.firebaseapp.com",
+  databaseURL: "https://haeseola-a83de-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "haeseola-a83de",
+  storageBucket: "haeseola-a83de.firebasestorage.app",
+  messagingSenderId: "103075772011",
+  appId: "1:103075772011:web:613534af89be2ac84ca32e"
+}
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
